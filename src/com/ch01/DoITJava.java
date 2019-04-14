fc4cd3d3b5904514cbca1ae7637fddbe03cf9f3d
kj0701 <appletak@nate.com>
2019-04-14 14:56:03 +0900

    init commit

---
+++ src/com/ch01/DoITJava.java
@@ -0,0 +1,14 @@
+package com.ch01;
+
+public class DoITJava {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println('a');
+		System.out.println("HI Hello JAVA");
+		System.out.println(100);
+		System.out.println(100.0);
+		System.out.println(false +" "+true);
+	}
+
+}

b26d7274d6f9ea867edb0c812f4ddcdff97b43d2
kj0701 <appletak@nate.com>
2019-04-14 17:18:50 +0900

    character

---
+++ src/com/ch02/Data01.java
@@ -0,0 +1,29 @@
+package com.ch02;
+
+public class Data01 {
+
+	public static void main(String[] args) {
+
+			// TODO Auto-generated method stub
+			boolean res = true;
+			System.out.println(res);
+			
+			char ch='a';
+			System.out.println(ch);
+			
+			ch='b';
+			System.out.println("ch="+ch);
+			
+			ch='οχ';
+			System.out.println("ch="+ch);
+
+			char k1 = '\uc548';
+			char k2 = '\ub155';
+			char k3 = '\ud558';
+			char k4 = '\uc138';
+			char k5 = '\uc694';
+			System.out.println(k1 +""+k2+""+k3+""+k4+""+k5);
+		
+	}
+
+}

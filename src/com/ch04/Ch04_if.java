bda71fde14d0a871dc36f00f510df9d14a710a42
kj0701 <appletak@nate.com>
2019-04-14 17:37:24 +0900

    한문자 입력 후 대소문자 확인

---
+++ src/com/ch04/Ch04_if.java
@@ -0,0 +1,15 @@
+package com.ch04;
+
+public class Ch04_if {
+	
+	public static void main(String[] args) {
+		int a=4;
+			if(a > 5) {
+				System.out.println("a는 5보다 크다");
+				
+			}else {
+				System.out.println("a는 5보다 작다");
+			}
+	}
+
+}
